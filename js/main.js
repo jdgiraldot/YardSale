ea910e3c76c987f661e6132358ac2a51b7b6406e
Jhonatan <jdgiraldot@outlook.com>
2023-09-04 00:37:27 -0500

    Añadidos los items de la tienda desde JavaScript

--- js/main.js
+++ js/main.js
@@ -1,19 +1,116 @@
 const hamburgerIcon = document.querySelector('#hamburger-icon')
-const mobileMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
-hamburgerIcon.addEventListener('click',toggleElement)
+const shoppingCartIcon = document.querySelector('#shopping-cart-icon')
+const shoppingCartMenu = document.querySelector('.shopping-cart-menu')
+
+hamburgerIcon.addEventListener('click', toggleElement)
+shoppingCartIcon.addEventListener('click', toggleElement)
 
 function toggleElement(e){
 
-    let openElement = document.querySelector('.active-left')    // Selecciona el elemento despegable desde la izquierda que esté activo.
-    if (openElement && !e.target) { openElement.classList.remove('active-left')}  // Sí existe un elemento abierto desde la izquierda y es diferente de e.target, entonces cierrelo.
-
-    openElement = document.querySelector('.active-rigth')
-    if (openElement && !e.target) { openElement.classList.remove('active-rigth')}
-
-    switch (e.target){
+switch (e.target){
         case hamburgerIcon: // Sí presionaron el icono hamburguesa se cambia el estado del menú desplegable en la versión mobile.
             mobileMenu.classList.toggle('active-left')
+            shoppingCartMenu.classList.remove('active-right')
+            break;
+        case shoppingCartIcon:
+            shoppingCartMenu.classList.toggle('active-right')
+            mobileMenu.classList.remove('active-left')
             break;
     }
 }
+
+// ------------ Lista de productos ------------ 
+
+const cardsContainer = document.querySelector('.cards-container');
+
+const productList = [];
+productList.push({
+    name:'Pokemón figure',
+    price: 120,
+    image: 'https://boomshock.co/cdn/shop/products/pokemon-select-action-figure-zapdos-15-cm.jpg?v=1663020989&width=1445'
+})
+productList.push ({
+    name:'Bike',
+    price: 12700,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1200,
+    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1600,
+    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1500,
+    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Seat',
+    price: 300,
+    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+});
+productList.push ({
+    name:'Tennis Montain Bike',
+    price: 2200,
+    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 800,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 600,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
+});
+productList.push ({
+    name:'Bicycle seat bag',
+    price: 876,
+    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+});
+
+
+productList.map(function (product){ 
+  const productCard = document.createElement('div');
+  productCard.classList.add('product-card');
+
+  const productImg = document.createElement('img');
+  productImg.classList.add('product-img')
+  productImg.setAttribute('src', product.image);
+
+  const productInfo = document.createElement('div');
+  productInfo.classList.add('product-info');  
+
+  const productInfoDiv = document.createElement('div');
+
+  const productPrice = document.createElement('p');
+  productPrice.innerText = '$' + product.price;
+  const productName = document.createElement('p');
+  productName.innerText = product.name;
+
+  productInfoDiv.appendChild(productPrice);
+  productInfoDiv.appendChild(productName);
+
+  const productInfoFigure = document.createElement('figure');
+  const productImgCart = document.createElement('img');
+  productInfo.classList.add('add-cart-icon');
+  productImgCart.setAttribute('src', './images/icons/bt_add_to_cart.svg');
+
+  productInfoFigure.appendChild(productImgCart);
+
+  productInfo.appendChild(productInfoDiv);
+  productInfo.appendChild(productInfoFigure);
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productInfo);
+
+  cardsContainer.appendChild(productCard);
+});
